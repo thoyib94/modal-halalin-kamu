f955ecf1f00272907bb6a493f2b10e4f084e1938
thoyib94 <43726585+thoyib94@users.noreply.github.com>
2020-06-20 00:46:03 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="forex-no-deposit-bonus-7-forex-trading.html"}
 if(x=="2"){xxx="5-tips-to-consider-before-investing-in.html"}
 if(x=="3"){xxx="bitcoin-and-binary-options-trading.html"}
-if(x=="4"){xxx="businesses-that-can-receive-financing.html"}a_to_ve[i].href="https://modal-halalin-kamu.blogspot.com/2020/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="businesses-that-can-receive-financing.html"}a_to_ve[i].href="https://download.bang2sutara.com/2020/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
